5d2ffc64698411932599f897140263fafcdf4a2d
emma <emmaogedegbe@gmail.com>
2020-04-15 06:25:25 +0100

    Updates 1

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-import covid19ImpactEstimator, { impactEstimatorOutput, regionData, populationData, hospitalBedsData, normalizeDays } from './estimator';
+import covid19ImpactEstimator, 
+{impactEstimatorOutput, regionData, populationData,
+  hospitalBedsData, normalizeDays } 
+  from './estimator';  
 
 function processForm(e) {
-    e.preventDefault();
-   const data = {
+  e.preventDefault();
+  const data = {
     pType: "Days",
     tElapse: normalizeDays(document.getElementById('periodType'),document.getElementById('timeToElapse')),
     rCases: document.getElementById('reportedCases'),
     pop: document.getElementById('population'),
     tBeds: document.getElementById('totalHospitalBeds')
-   }
-
-    const imputData = covid19ImpactEstimator({
-        region: regionData, 
-        periodType: data.pType,
-        timeToElapse: data.tElapse, 
-        reportedCases: data.rCases,
-        population: data.pop, 
-        totalHospitalBeds: data.tBeds 
-    });
-
-    const outputData = impactEstimatorOutput(imputData);
-    console.log(outputData);
-
 }
 
-document.addEventListener('DOMContentLoaded', (e) =>{
-  const impForm = document.getElementById('impactForm');
+ const imputData = covid19ImpactEstimator({
+    region: regionData, 
+    periodType: data.pType,
+    timeToElapse: data.tElapse, 
+    reportedCases: data.rCases,
+    population: data.pop, 
+    totalHospitalBeds: data.tBeds 
+ });
+
+ const outputData = impactEstimatorOutput(imputData);
+  
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+ const impForm = document.getElementById('impactForm');
   document.getElementById('population').value = populationData.getDefaultPopulation();
   document.getElementById('totalHospitalBeds').value = hospitalBedsData.getDefaultBeds();
   impForm.addEventListener('submit', processForm(e));
-
 });
 
 
