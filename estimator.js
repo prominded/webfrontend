9f6296e1fa8354a4e47f8b75be4f39fd6d040e4c
emma <emmaogedegbe@gmail.com>
2020-04-16 09:03:56 +0100

    Update this object

--- estimator.js
+++ estimator.js
@@ -29,21 +29,21 @@ const impactEstimatorOutput = (data) => ({
   inputeData: data,
   impact: {
     currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: this.currentlyInfected * (2 ** (data.timeToElapse / 3)),
-    severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
+    infectionsByRequestedTime: impactEstimatorOutput.impact.currentlyInfected * (2 ** (data.timeToElapse / 3)),
+    severeCasesByRequestedTime: impactEstimatorOutput.impact.infectionsByRequestedTime * 0.15,
     hospitalBedsByRequestedTime: data.totalHospitalBeds * 0.35,
-    casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
-    casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
-    dollarsInFlight: this.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
+    casesForICUByRequestedTime: impactEstimatorOutput.impact.infectionsByRequestedTime * 0.05,
+    casesForVentilatorsByRequestedTime: impactEstimatorOutput.impact.infectionsByRequestedTime * 0.02,
+    dollarsInFlight: impactEstimatorOutput.impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
 },
   severeImpact: {
     currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: this.currentlyInfected * (2 ** (data.timeToElapse / 3)),
-    severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
+    infectionsByRequestedTime: impactEstimatorOutput.severeImpact.currentlyInfected * (2 ** (data.timeToElapse / 3)),
+    severeCasesByRequestedTime: impactEstimatorOutput.severeImpact.infectionsByRequestedTime * 0.15,
     hospitalBedsByRequestedTime: data.totalHospitalBeds * 0.35,
-    casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
-    casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
-    dollarsInFlight: this.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
+    casesForICUByRequestedTime: impactEstimatorOutput.severeImpact.infectionsByRequestedTime * 0.05,
+    casesForVentilatorsByRequestedTime: impactEstimatorOutput.severeImpact.infectionsByRequestedTime * 0.02,
+    dollarsInFlight: impactEstimatorOutput.severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
 }
 });
 
